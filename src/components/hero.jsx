f5e1ce1d1cd590d20b416fc86f6ba4a8513ffa87
talhaticx <mtalhaayaz2005@gmail.com>
2025-02-03 00:17:41 +0500

    hero ready

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { Spotlight } from "./A-ui/spotlight-new";
 import GridLines from "react-gridlines";
-import Title from "./title";
 
 export default function Hero() {
   return (
@@ -11,20 +10,23 @@ export default function Hero() {
       cellWidth={30}
       cellWidth2={30}
       strokeWidth={1}
-
       lineColor="#0D1116"
       lineColor2="#0D1116"
     >
-      <div className="h-screen w-screen  flex md:items-center md:justify-center bg-black/[0.60] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight duration={15} />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Electrocon 24 <br /> The Most Aviated Event
-        </h1>
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          Technical Event Organazied by IET on Campus UET Lahore. Modules like Coding Competitions, Gaming and Robo War.
-        </p>
-      </div>
+      <div className="h-screen w-screen  flex md:items-center md:justify-center bg-black/[0.70] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+        <Spotlight duration={15} />
+        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+          <h1 className="text-4xl md:text-7xl TitleFontCustom text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+            Electrocon 25
+          </h1>
+          <h3 className="text-3xl mt-4 md:mt-0 md:text-5xl TitleFontCustom text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+            The Most Aviated Event
+          </h3>
+          <p className="mt-4 md:mt-5 text-lg md:text-base font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+            Technical Event Organazied by IET on Campus UET Lahore. Modules like
+            Coding Competitions, Gaming and Robo War.
+          </p>
+        </div>
       </div>
     </GridLines>
   );
