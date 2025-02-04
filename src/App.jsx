a72c9c49f3554d9b10f8b173bdd757505e82190f
talhaticx <mtalhaayaz2005@gmail.com>
2025-02-04 13:18:15 +0500

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import Hero from './components/hero'
+import React from "react";
+import Hero from "./pages/hero";
+import Home from "./pages/home";
 
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Hero/>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Hero />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
