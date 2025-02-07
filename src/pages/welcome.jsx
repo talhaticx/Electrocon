3b72d488224a1d2b0b1ee09681082b68091a0f5a
talhaticx <mtalhaayaz2005@gmail.com>
2025-02-07 23:11:43 +0500

    welcome animation added

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -2,7 +2,10 @@
 import React from "react";
 import { Spotlight } from "../ui/spotlight-new";
 import GridLines from "react-gridlines";
+import { motion } from "framer-motion"; // Import Framer Motion
 import Button from "../components/button";
+
+import { ECON } from "../data/econ";
 
 export default function Welcome() {
   return (
@@ -16,17 +19,25 @@ export default function Welcome() {
     >
       <div className="h-screen w-screen flex md:items-center md:justify-center bg-black/[0.70] antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <Spotlight duration={15} />
-        <div className=" p-4 max-w-7xl mx-auto relative z-10  w-full pt-20 md:pt-0 flex flex-col items-center gap-4">
+        <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0 flex flex-col items-center gap-4">
           <div>
-            <h1 className="text-4xl md:text-7xl TitleFontCustom text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-              Electrocon 25
-            </h1>
-            <h3 className="text-3xl mt-4 md:mt-0 md:text-5xl TitleFontCustom text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+            <motion.h1
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.2, ease: "easeOut" }}
+              className="text-4xl md:text-7xl TitleFontCustom text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50"
+            >
+              {ECON}
+            </motion.h1>
+            <motion.h3
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 2.2, ease: "easeOut" }} className="text-3xl mt-4 md:mt-0 md:text-5xl TitleFontCustom text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
               The Most Aviated Event
-            </h3>
+            </motion.h3>
           </div>
           <p className="mt-4 md:mt-5 text-lg md:text-base font-normal text-neutral-300 max-w-lg text-center mx-auto">
-            Technical Event Organazied by IET on Campus UET Lahore. Modules like
+            Technical Event Organized by IET on Campus UET Lahore. Modules like
             Coding Competitions, Gaming and Robo War.
           </p>
           <div className="flex justify-center mt-4">
